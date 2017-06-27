bb369a0ec7617f94e9bb35212de4dd4a2b4b091e
Parag Chauhan <paragchauhan99799>
2017-06-28 04:06:44 +0530

    mongoDB connections

--- setup/index.js
+++ setup/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/senzopt',
+    MONGODB_URI: 'mongodb://parag99799:parag99799@ds139352.mlab.com:39352/task' || 'mongodb://localhost/senzopt',
     PORT: process.env.PORT || 3000,
 }
